15f2e9dab091da5d41bd74f2fd3c98fcf6aa3f79
Jhonmar Morales <jhonmar.morales.jr@gmail.com>
2026-06-02 20:52:30 -0400

    Add reportes module and integrate GestorReportes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod estructuras;
 mod input;
 mod token;
+mod reportes;
 
+use reportes::GestorReportes;
 fn main() {
+    let mut gestor = GestorReportes::new();
+    
     let ejemplo = token::Token::nuevo(token::TipoToken::Keyword, "int", 1, 1);
     println!("Contrato Token: {ejemplo}");
+
+    gestor.registrar_token(ejemplo);
+
+    gestor.imprimir_reporte_final();
 }
